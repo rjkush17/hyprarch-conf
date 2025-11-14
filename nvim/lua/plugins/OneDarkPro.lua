1897915af18c1b22c3acc335e2a8e32a0cb81855
rjkush17 <rjkush17rishabh@gmail.com>
2025-11-14 20:52:07 +0530

    update font styling in the conf

--- nvim/lua/plugins/OneDarkPro.lua
+++ nvim/lua/plugins/OneDarkPro.lua
@@ -1,20 +1,35 @@
 return {
-  "olimorris/onedarkpro.nvim",
-  lazy = false,    -- Load immediately
-  priority = 1000, -- Ensure it loads before other themes
-  config = function()
-    require("onedarkpro").setup({
-      colors = {}, -- Override colors if needed
-      highlights = {
-        keywords = { style = "bold" },
-        strings = { style = "italic" },
-        functions = { style = "italic" }
-      },
-      options = {
-        transparency = true, -- Set true for transparent background
-        terminal_colors = true,
-      },
-    })
-    vim.cmd("colorscheme onedark")
-  end,
+	"olimorris/onedarkpro.nvim",
+	lazy = false, -- Load immediately
+	priority = 1000, -- Ensure it loads before other themes
+	config = function()
+		require("onedarkpro").setup({
+			colors = {}, -- Override colors if needed
+			highlights = {
+				keywords = { style = "bold" },
+				strings = { style = "italic" },
+				functions = { style = "italic" },
+			},
+			styles = {
+				types = "bold",
+				methods = "NONE",
+				numbers = "NONE",
+				strings = "italic",
+				comments = "italic",
+				keywords = "bold,italic",
+				constants = "NONE",
+				functions = "bold",
+				operators = "NONE",
+				variables = "bold",
+				parameters = "italic",
+				conditionals = "italic",
+				virtual_text = "italic",
+			},
+			options = {
+				transparency = true, -- Set true for transparent background
+				terminal_colors = true,
+			},
+		})
+		vim.cmd("colorscheme onedark")
+	end,
 }
